5fb6a09e97ca1e1b2ae18c305c93ec6b26f7f45b
digildev1 <152153973+digildev1@users.noreply.github.com>
2024-01-29 16:05:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ dotenv.config()
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://icreation.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
